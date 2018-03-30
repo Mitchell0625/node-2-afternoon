d3254ffda3c96eda53955757f06743d55b29e3ec
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-03-30 11:40:04 -0500

    completed afternoon project

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(express.static("././client"));
+
 massive(process.env.CONNECTION_STRING).then(dbInstance => {
   // console.log(dbInstance);
   app.set("db", dbInstance);
